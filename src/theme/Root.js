9f0e1fe32849f4d7c69b3664efbe35b4c26c89c7
Mihai Muresan <mihai.muresan@twipla.com>
2025-06-11 12:56:26 +0200

    Use twipla worker

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {LogLevel, TwiplaWorker} from "@twipla/worker";
 
 // Default implementation, that you can customize
 export default function Root({children}) {
+    useEffect(() => {
+        const twiplaWorker = new TwiplaWorker({
+            websiteId: 'ce671b0e-46ae-11f0-a7ae-960004340fd3',
+            logLevel: LogLevel.ERROR,
+            env: 'production'
+        });
+        twiplaWorker.run({ tags: { test: 'test_value' } })
+    }, []);
+
     return <>
         {children}
-
-        <script dangerouslySetInnerHTML={{ __html: "(function(v,i,s,a,t){v[t]=v[t]||function(){(v[t].v=v[t].v||[]).push(arguments)};if(!v._visaSettings){v._visaSettings={}}v._visaSettings[a]={v:'1.0',s:a,a:'1',t:t};var b=i.getElementsByTagName('body')[0];var p=i.createElement('script');p.defer=1;p.async=1;p.src=s+'?s='+a;b.appendChild(p)})(window,document,'//app-worker.visitor-analytics.io/main.js','ce671b0e-46ae-11f0-a7ae-960004340fd3','va')" }} />
     </>;
 }
